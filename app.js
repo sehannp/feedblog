4c806fcce42ab08334ba8d1570731c61cd58d3ca
sehannp <sehanp@gmail.com>
2020-01-28 19:25:51 +1100

    updated to send feeds and add feed

--- app.js
+++ app.js
@@ -3,13 +3,45 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const MONGODB_URI = require('./env/mongoose');
-
+const multer = require('multer');
 
 const bodyParser = require('body-parser');
+// const fileStorage = multer.diskStorage({
+//     destination: (req,res,cb) => {
+//         cb(null, 'images');
+//     },
+//     filename: (req,res,cb) => {
+//         cb(null, new Date().toISOString() + '-' + file.originalname)
+//     }
+// })
+const uuidv4 = require('uuid/v4')
+ 
+const fileStorage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function(req, file, cb) {
+        cb(null, uuidv4())
+    }
+});
+
+const fileFilter = (req,file,cb) => {
+    if (file.mimetype === 'images/png' || 
+        file.mimetype === 'image/jpg' || 
+        file.mimetype === 'image/jpeg')
+    {
+            cb(null,true);
+    }
+    else{
+        cb(null,false);
+    }
+};
 
 const feedRoutes = require('./routes/feed');
 app.get('/favicon.ico', (req, res) => res.status(204));
 app.use(bodyParser.json()); 
+app.use(multer({storage:fileStorage, fileFilter: fileFilter}).single('image'));
+
 app.use('/images',express.static(path.join(__dirname,'images')));
 
 app.use((req, res, next) => {
--- controllers/feed.js
+++ controllers/feed.js
@@ -3,18 +3,19 @@ const {validationResult} = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
+  Post.find()
+  .then(posts => {
     res.status(200).json({
-      posts: [{
-        _id: '1',
-        title: 'First Post', 
-        content: 'This is the first post!',
-        imageUrl: 'images/knight.png',
-        creator: {
-          name: 'Sehan'
-        },
-        createdAt: new Date()
-      }]
+      message: 'fetched posts successfully',
+      posts: posts
     });
+  })
+  .catch(err => {
+    if(!err.statusCode){
+      err.statusCode = 500;
+    }
+    next(err);
+  });
 };
 
 exports.createPost = (req, res, next) => {
@@ -25,11 +26,17 @@ exports.createPost = (req, res, next) => {
       error.statusCode = 422;
       throw error;
     }
+    if(!req.file){
+      const error = new Error('No image provided/invalid file format');
+      error.statusCode = 422;
+      throw error;
+    }
+    const imageUrl = req.file.path.replace("\\" ,"/");
     const {title,content} = req.body
     const post = new Post({
       title, 
       content,
-      imageUrl: 'images/knight.png',
+      imageUrl,
       creator: {name: 'Sehan'}
     })
     post.save()
@@ -45,9 +52,63 @@ exports.createPost = (req, res, next) => {
       if(!err.statusCode){
         err.statusCode = 500;
       }
-      //throw wont work
       next(err);
     });
-
-
 };
+
+exports.getPost = (req,res,next) => {
+  const postId = req.params.postId;
+
+  Post.findById({_id: postId})
+  .then(post => {
+    if(!post){
+      const error = new Error('Could not find post');
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({
+      message: 'Post fetched',
+      post
+    });
+  })
+  .catch(err => {
+    if(!err.statusCode){
+      err.statusCode = 500;
+    }
+    next(err);
+  });
+}
+// exports.updatePost = (req,res,next) => {
+//   const postId = req.params.postId;
+//   const errors = validationResult(req);
+    
+//   if (!errors.isEmpty()){
+//     const error = new Error('Validation Failed, entered data is incorrect');
+//     error.statusCode = 422;
+//     throw error;
+//   }
+
+//   const {title, content} = req.body;
+//   let imageUrl = req.body.image;
+
+//   if (req.file){
+//     imageUrl = req.file.path.replace("\\","/");
+//   }
+//   if (!imageUrl){
+//     const error = new Error('No file picked');
+//     error.statusCode = 442;
+//     throw error;
+//   }
+//   Post.findById(postId)
+//   .then(post => {
+//     if(!post){
+      
+//     }
+//   })
+//   .catch(err => {
+//     if(!err.statusCode){
+//       err.statusCode = 500;
+//     }
+//     next(err);
+//   })
+// }
