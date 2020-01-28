aa4563ed84f92f04add44c0b3d555ee23361fe92
sehannp <sehanp@gmail.com>
2020-01-28 20:09:03 +1100

    delete post routes added

--- controllers/feed.js
+++ controllers/feed.js
@@ -133,6 +133,36 @@ exports.updatePost = (req,res,next) => {
   })
 }
 
+
+exports.deletePost = (req,res,next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+  .then(post => {
+    if(!post){
+      const error = new Error('Could not find post');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    //TODO: check logged in user is the owner ?
+    clearImage(post.imageUrl);
+    return Post.findByIdAndRemove(postId);
+  })
+  .then(result => {
+    console.log(result);
+    res.status(200).json({
+      message: 'Deleted Post!'
+    });
+  })
+  .catch(err => {
+    if(!err.statusCode){
+      err.statusCode = 500;
+    }
+    next(err);
+  })
+}
+
 const clearImage = filePath => {
   filePath = path.join(__dirname, '..', filePath);
   fs.unlink(filePath, err => console.log(err));
--- routes/feed.js
+++ routes/feed.js
@@ -20,4 +20,7 @@ router.put('/post/:postId',[
     body('title').trim().isLength({min:5}),
     body('content').trim().isLength({min:5})
 ], feedController.updatePost);
+
+router.delete('/post/:postId', feedController.deletePost);
+
 module.exports = router;
