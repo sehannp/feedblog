4fc56d30b0b7bfc1edf83c30eeec73e45ff6611b
sehannp <sehanp@gmail.com>
2020-01-28 19:42:04 +1100

    post updating logic added

--- controllers/feed.js
+++ controllers/feed.js
@@ -78,37 +78,50 @@ exports.getPost = (req,res,next) => {
     next(err);
   });
 }
-// exports.updatePost = (req,res,next) => {
-//   const postId = req.params.postId;
-//   const errors = validationResult(req);
+exports.updatePost = (req,res,next) => {
+  const postId = req.params.postId;
+  const errors = validationResult(req);
     
-//   if (!errors.isEmpty()){
-//     const error = new Error('Validation Failed, entered data is incorrect');
-//     error.statusCode = 422;
-//     throw error;
-//   }
+  if (!errors.isEmpty()){
+    const error = new Error('Validation Failed, entered data is incorrect');
+    error.statusCode = 422;
+    throw error;
+  }
 
-//   const {title, content} = req.body;
-//   let imageUrl = req.body.image;
+  const {title, content} = req.body;
+  let imageUrl = req.body.image;
 
-//   if (req.file){
-//     imageUrl = req.file.path.replace("\\","/");
-//   }
-//   if (!imageUrl){
-//     const error = new Error('No file picked');
-//     error.statusCode = 442;
-//     throw error;
-//   }
-//   Post.findById(postId)
-//   .then(post => {
-//     if(!post){
-      
-//     }
-//   })
-//   .catch(err => {
-//     if(!err.statusCode){
-//       err.statusCode = 500;
-//     }
-//     next(err);
-//   })
-// }
+  if (req.file){
+    imageUrl = req.file.path.replace("\\","/");
+  }
+  if (!imageUrl){
+    const error = new Error('No file picked');
+    error.statusCode = 442;
+    throw error;
+  }
+
+  Post.findById(postId)
+  .then(post => {
+    if(!post){
+      const error = new Error('Could not find post');
+      error.statusCode = 404;
+      throw error;
+    }
+    post.title = title;
+    post.imageUrl = imageUrl;
+    post.content = content;
+    return post.save();
+  })
+  .then(result => {
+    res.status(200).json({
+      message: 'post updated!',
+      post: result
+    })
+  })
+  .catch(err => {
+    if(!err.statusCode){
+      err.statusCode = 500;
+    }
+    next(err);
+  })
+}
